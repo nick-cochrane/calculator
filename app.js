24b7ac325de122471148e190386d86da59056094
Nick Cochrane <ncochrane395@gmail.com>
2023-02-17 04:17:28 -0800

    html structure set, basic css styling, and embedded audio and gif files

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello!")
+function add (x, y) {
+    return x + y;
+}
+
+function subract (x, y) {
+    return x - y;
+}
+
+function multiple (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    return x / y;
+}
+
+function operate (operator, x, y) {
+    return operator(x, y);
+}
+
