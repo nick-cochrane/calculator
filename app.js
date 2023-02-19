dfa168be2f32bd55b3cabd08d9e966dd299441b0
Nick Cochrane <ncochrane395@gmail.com>
2023-02-19 00:52:51 -0800

    got initial operator keys functional, rearranged keys

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
+const displayPane = document.querySelector('#calc-screen');
+
+let calculatorState = {
+    firstNumber: null,
+    secondNumber: null,
+    operator: ''
+}
+
+const buttons = [...document.querySelectorAll('.number-button')];
+buttons.forEach(button => button.addEventListener("click", numberButtonClick));
+
+function numberButtonClick (e) {
+    currentDisplay = displayPane.textContent;
+    displayPane.textContent = currentDisplay + e.target.textContent;
+}
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener("click", function (e) {
+    displayPane.textContent = null;
+});
+
+const operatorButtons = [...document.querySelectorAll('.operator-button')];
+operatorButtons.forEach(button => button.addEventListener("click", operatorButtonClick));
+
+function operatorButtonClick (e) {
+    calculatorState.firstNumber = parseFloat(displayPane.textContent);
+    displayPane.textContent = null;
+    calculatorState.operator = e.target.id;
+    console.log(calculatorState);
+}
+
+const equalsButton = document.querySelector('#equals-button');
+equalsButton.addEventListener("click", function (e) {
+    calculatorState.secondNumber = parseFloat(displayPane.textContent);
+    result = operate(window[calculatorState.operator], calculatorState.firstNumber, calculatorState.secondNumber);
+    displayPane.textContent = result;
+    calculatorState.firstNumber = result;
+    calculatorState.secondNumber = null;
+});
+
+const deleteButton = document.querySelector('#delete-button');
+deleteButton.addEventListener("click", function (e) {
+    displayPane.textContent = null;
+    calculatorState = {
+        firstNumber: null,
+        secondNumber: null,
+        operator: ''
+    }
+});
+
+
+
 function add (x, y) {
     return x + y;
 }
 
-function subract (x, y) {
+function subtract (x, y) {
     return x - y;
 }
 
-function multiple (x, y) {
+function multiply (x, y) {
     return x * y;
 }
 
